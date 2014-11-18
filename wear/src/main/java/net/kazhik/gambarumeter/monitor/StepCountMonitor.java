8ceebacd3697135831d629efc6e37e6f87ea08b6
Koike Kazuhiko <kazhik@gmail.com>
2014-11-19 06:40:06 +0900

    delete detailed step counter

--- wear/src/main/java/net/kazhik/gambarumeter/monitor/StepCountMonitor.java
+++ wear/src/main/java/net/kazhik/gambarumeter/monitor/StepCountMonitor.java
@@ -9,13 +9,11 @@ import android.hardware.SensorManager;
  * Created by kazhik on 14/10/12.
  */
 public class StepCountMonitor implements SensorEventListener {
-    private static int SENSORTYPE_DETAILED_STEP_COUNTER = 65537;
 
     private SensorManager sensorManager;
     private SensorValueListener listener;
 
     private Sensor stepCountSensor;
-    private Sensor detailedStepCountSensor;
     private int initialValue = 0;
     private SensorValue currentValue = new SensorValue();
 
@@ -24,7 +22,6 @@ public class StepCountMonitor implements SensorEventListener {
         this.sensorManager = sensorManager;
         this.listener = listener;
         this.stepCountSensor = this.sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
-        this.detailedStepCountSensor = this.sensorManager.getDefaultSensor(SENSORTYPE_DETAILED_STEP_COUNTER);
 
     }
     public int getStepCount() {
@@ -33,12 +30,10 @@ public class StepCountMonitor implements SensorEventListener {
     public void start() {
         this.initialValue = 0;
         this.sensorManager.registerListener(this, this.stepCountSensor, SensorManager.SENSOR_DELAY_NORMAL);
-        this.sensorManager.registerListener(this, this.detailedStepCountSensor, SensorManager.SENSOR_DELAY_NORMAL);
 
     }
     public void stop() {
         this.sensorManager.unregisterListener(this, this.stepCountSensor);
-        this.sensorManager.unregisterListener(this, this.detailedStepCountSensor);
 
     }
 
@@ -52,22 +47,14 @@ public class StepCountMonitor implements SensorEventListener {
             default:
                 return;
         }
-        if (sensorEvent.sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
-            int newValue = (int)sensorEvent.values[0];
-            if (this.initialValue == 0) {
-                this.initialValue = newValue;
-            } else if (newValue != this.currentValue.getValue()) {
-                this.listener.onStepCountChanged(sensorEvent.timestamp, newValue - this.initialValue);
-            }
-            this.currentValue.setTimestamp(sensorEvent.timestamp);
-            this.currentValue.setValue(newValue);
-        } else if (sensorEvent.sensor.getType() == SENSORTYPE_DETAILED_STEP_COUNTER) {
-            for (int i = 0; i < sensorEvent.values.length; i++) {
-                if (sensorEvent.values[i] != 0) {
-//                    System.out.println("detailed step counter value[" + i + "]: " + sensorEvent.values[i]);
-                }
-            }
+        int newValue = (int)sensorEvent.values[0];
+        if (this.initialValue == 0) {
+            this.initialValue = newValue;
+        } else if (newValue != this.currentValue.getValue()) {
+            this.listener.onStepCountChanged(sensorEvent.timestamp, newValue - this.initialValue);
         }
+        this.currentValue.setTimestamp(sensorEvent.timestamp);
+        this.currentValue.setValue(newValue);
     }
 
     @Override
